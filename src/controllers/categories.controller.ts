a39933f17b99f2af9d958526e8cea6ff7ce1d917
CaioMiranda12 <caioclm04@gmail.com>
2024-01-08 12:43:44 -0300

    first-commit

---
+++ src/controllers/categories.controller.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+
+import { CategoriesRepository } from '../database/repositories/categories.repository';
+import { CategoryModel } from '../database/schemas/category.schema';
+import { CategoriesService } from '../services/categories.service';
+
+export class CategoriesController {
+  async create(_: Request, res: Response) {
+    const repository = new CategoriesRepository(CategoryModel);
+
+    const service = new CategoriesService(repository);
+    const result = await service.create();
+
+    return res.status(201).json(result);
+  }
+}
---
+++ src/services/categories.service.ts
@@ -0,0 +1,17 @@
+import { CategoriesRepository } from '../database/repositories/categories.repository';
+import { Category } from '../entities/category.entity';
+
+export class CategoriesService {
+  constructor(private categoriesRepository: CategoriesRepository) {}
+
+  async create(): Promise<Category> {
+    const category = new Category({
+      title: 'Example Category',
+      color: '#ff33bb',
+    });
+
+    const createdCategory = await this.categoriesRepository.create(category);
+
+    return createdCategory;
+  }
+}
